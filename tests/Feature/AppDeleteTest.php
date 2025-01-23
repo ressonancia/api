3adf606976140d04cc01d05cd10cd0dad6089f39
Leonardo Lemos <leonardo.lemos@convenia.com.br>
2025-01-23 07:48:38 -0300

    fix tests

--- tests/Feature/AppDeleteTest.php
+++ tests/Feature/AppDeleteTest.php
@@ -3,10 +3,10 @@
 use App\Models\App;
 use Illuminate\Auth\Middleware\Authenticate;
 use Illuminate\Http\Response;
-use Illuminate\Support\Str;
 
 test('user can delete an app', function () {
     $app = App::factory()->create();
+    $appToKeep = App::factory()->create();
 
     $response = $this->deleteJson(route('api.apps.destroy', $app->id));
     $response->assertStatus(Response::HTTP_NO_CONTENT);
@@ -16,10 +16,15 @@ test('user can delete an app', function () {
         'deleted_at' => null
     ]);
 
-});
+    $this->assertDatabaseHas('apps', [
+        'id' => $appToKeep->id,
+        'deleted_at' => null
+    ]);
+
+})->only();
 
 
-test('user needs to be logged in to create app', function () {
+test('user needs to be logged in to delete an app', function () {
     $this->withMiddleware(Authenticate::class);
 
     $this->deleteJson(route('api.apps.destroy', ['app' => 1]))->assertUnauthorized();
