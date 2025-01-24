0fa703f0ed80a5604b583415abe2874a1f50e3cc
Leonardo Lemos <leonardo.lemos@convenia.com.br>
2025-01-24 20:34:38 -0300

    Force schema to allow TLS termination

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,6 +29,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         resolve(ServerProviderManager::class)->extend('database', function () {
             return new DatabaseApplicationProvider(
                 App::get()->collect()
