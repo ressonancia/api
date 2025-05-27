3082c5838288701eae3d5f7d9caa534e76620365
Leonardo Lemos <leonardo.lemos@convenia.com.br>
2025-05-26 21:39:47 -0300

    refresh DatabaseApplicationProvider from IoC

--- app/Ressonance/Http/Controllers/RefreshApplicationsController.php
+++ app/Ressonance/Http/Controllers/RefreshApplicationsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Ressonance\Http\Controllers;
 
+use App\Ressonance\DatabaseApplicationProvider;
 use Laravel\Reverb\ApplicationManager;
 use Laravel\Reverb\Servers\Reverb\Http\Response;
 use Laravel\Reverb\Protocols\Pusher\Http\Controllers\Controller;
@@ -14,6 +15,7 @@ class RefreshApplicationsController extends Controller
     public function __invoke(): Response
     {
         app()->make(ApplicationManager::class)->forgetDrivers();
+        app()->forgetInstance(DatabaseApplicationProvider::class);
 
         return new Response((object) ['status' => 'Applications Refreshed']);
     }
