3608983754ad3fca4c56724a8d943380584fdc6f
Leonardo Lemos <leonardo.lemos@convenia.com.br>
2026-06-25 23:12:08 -0300

    fix image path

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="data:image/png;base64,{{ base64_encode(file_get_contents(resource_path('img/logo-text.png'))) }}" alt="{{ config('app.name') }}" style="max-height: 40px; width: auto;">
+<img src="https://objectstorage.sa-saopaulo-1.oraclecloud.com/n/grn4rsa6imo3/b/ressonance-assets/o/logo-text.png" alt="{{ config('app.name') }}" style="max-height: 40px; width: auto;">
 </a>
 </td>
 </tr>
