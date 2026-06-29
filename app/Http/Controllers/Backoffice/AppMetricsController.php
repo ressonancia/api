2280265fe132aa35be089b2a80b62d4df8ed3191
Victor Gazotti <vtr.gomes@hotmail.com>
2026-06-28 23:35:38 -0300

    trying to get connection

--- app/Http/Controllers/Backoffice/AppMetricsController.php
+++ app/Http/Controllers/Backoffice/AppMetricsController.php
@@ -5,44 +5,66 @@ namespace App\Http\Controllers\Backoffice;
 use App\Http\Controllers\Controller;
 use App\Models\App;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Client\Response;
 
 class AppMetricsController extends Controller
 {
     public function index(): JsonResponse
     {
         $apps = App::with('user')->get();
-
         $metrics = [];
-        $startOfMonthTimestamp = Carbon::now()->startOfMonth()->getTimestamp();
-
-        $appIds = $apps->pluck('app_id')->toArray();
-
-        $allConnections = DB::table('pulse_values')
-            ->where('type', 'reverb_connections')
-            ->whereIn('key', $appIds)
-            ->pluck('value', 'key');
-
-        $allMessages = DB::table('pulse_aggregates')
-            ->where('type', 'reverb_message')
-            ->whereIn('key', $appIds)
-            ->where('bucket', '>=', $startOfMonthTimestamp)
-            ->where('aggregate', 'count')
-            ->groupBy('key')
-            ->selectRaw('\'key\', sum(value) as total')
-            ->pluck('total', 'key');
 
         foreach ($apps as $app) {
+            $connections = 0;
+
+            try {
+                $connectionsResponse = $this->signedReverbRequest($app);
+                if ($connectionsResponse->successful()) {
+                    $connectionsData = $connectionsResponse->json();
+                    $connections = $connectionsData['connections'] ?? 0;
+                }
+            } catch (\Exception $e) {}
+
             $metrics[] = [
                 'app_name' => $app->app_name,
                 'app_id' => $app->app_id,
-                'user_email' => $app->user ? $app->user->email : null,
-                'current_connections' => (int) ($allConnections[$app->app_id] ?? 0),
-                'messages_sent' => (int) ($allMessages[$app->app_id] ?? 0),
+                'user_mail' => $app->user ? $app->user->email : null,
+                'current_connections' => (int) $connections,
+                'messages_sent' => 0,
             ];
         }
 
         return response()->json($metrics);
+    }
+
+    private function signedReverbRequest(App $app): Response
+    {
+        $path = "/apps/{$app->app_id}/connections";
+        $method = 'GET';
+        $params = [];
+        $key = $app->app_key;
+        $secret = $app->app_secret;
+        $host = config('reverb.servers.reverb.host');
+        $port = config('reverb.servers.reverb.port');
+        $scheme = config('app.force_schema', false) ? 'https' : 'http';
+
+        $params['auth_key'] = $key;
+        $params['auth_timestamp'] = time();
+        $params['auth_version'] = '1.0';
+
+        ksort($params);
+
+        $stringToSign = implode("\n", [
+            strtoupper($method),
+            $path,
+            http_build_query($params),
+        ]);
+
+        $params['auth_signature'] = hash_hmac('sha256', $stringToSign, $secret);
+
+        $url = "{$scheme}://{$host}:{$port}{$path}?" . http_build_query($params);
+
+        return Http::get($url);
     }
 }
